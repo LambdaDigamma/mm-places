c3cf9919a86e45d75bdbc34469a528b030fb0f6f
Lennart Fischer <github@lambdadigamma.com>
2021-01-22 17:31:26 +0100

    Fixed Route

--- routes/admin.php
+++ routes/admin.php
@@ -1,12 +1,12 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use LambdaDigamma\MMPlaces\Controllers\PlaceController;
+use LambdaDigamma\MMPlaces\Http\Controllers\PlaceController;
 
 /**
  * ------------------------------
  * Places General
  * ------------------------------
  */
- 
+
 Route::put('/places/{anyplace}', [PlaceController::class, 'update'])->name('places.update');
